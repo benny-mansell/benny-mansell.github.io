f3a2c660f203a6e36e72944407c8947e146f00d1
Emma Mansell <bennymansell40@gmail.com>
2024-09-13 12:57:19 -0400

    hmm

--- app/components/video.tsx
+++ app/components/video.tsx
@@ -3,7 +3,7 @@ import React, { useState } from "react";
 
 const Video = ({ src, content }: { src: string; content: any }) => {
   return (
-    <div className="flex flex-col lg:flex-row justify-center items-center mt-10 px-5 py-10">
+    <div className="flex flex-col lg:flex-row justify-center items-center mt-10 px-5 pt-10">
       <iframe
         className="w-[300px] h-[168px] sm:w-[400px] sm:h-[225px] md:h-[300px] md:w-[533px] max-w-screen aspect-video"
         src={src}
