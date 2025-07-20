c1b5681c1cd60fff6ace879e091ea0d08e39b8f0
Benny Mansell <bennymansell49@gmail.com>
2025-07-20 14:18:11 -0700

    editing camera icon

--- app/components/nav.tsx
+++ app/components/nav.tsx
@@ -60,12 +60,15 @@ export default function Nav() {
     <div className="mt-10 mb-10 lg:mt-64">
       <div className="flex flex-col items-center justify-between h-16">
         <div className="flex flex-col justify-center items-center py-2 text-m text-zinc-100">
-          <span className="text-4xl font-bold text-white p-5 text-center font-['Times_New-Roman',_Times,_serif]">
+          <span className="text-4xl font-bold text-white p-5 text-center">
             <a href="/">BENNY MANSELL </a>
           </span>
           <span className="flex flex-row space-x-4">
             <a href="https://www.linkedin.com/in/benny-mansell/">
               <img alt="Linkedin" src="./linkedin.svg" />
+            </a>
+            <a href="/Photography">
+              <img alt="photo" src="./photo.svg" />
             </a>
             <a href="mailto:bennymansell49@gmail.com">
               <img alt="Email" src="./email.svg" />
