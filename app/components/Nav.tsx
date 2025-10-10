b39bab4bb82d97a650f3e1e2ca126320899f4104
Emma Mansell <73774046+7emansell@users.noreply.github.com>
2025-10-10 11:56:30 -0400

    Hmmm

---
+++ app/components/Nav.tsx
@@ -0,0 +1,87 @@
+"use client";
+import React, { useState } from "react";
+
+export default function Nav() {
+  const [accordions, setAccordion] = useState([
+    {
+      key: 1,
+      title: "CONTACT",
+      data: (
+        <div className="flex flex-row justify-center space-x-4">
+          <a href="https://www.linkedin.com/in/benny-mansell/">
+            <img alt="Linkedin" src="./linkedin.svg" />
+          </a>
+          <a href="">
+            <img alt="Email" src="./email.svg" />
+          </a>
+        </div>
+      ),
+      isOpen: false,
+    },
+  ]);
+  const toggleAccordion = (accordionkey: number) => {
+    const updatedAccordions = accordions.map((accord) => {
+      if (accord.key === accordionkey) {
+        return { ...accord, isOpen: !accord.isOpen };
+      } else {
+        return { ...accord, isOpen: false };
+      }
+    });
+
+    setAccordion(updatedAccordions);
+  };
+
+  function Accordion(props: {
+    toggleAccordion: React.MouseEventHandler<HTMLButtonElement>;
+    title: string;
+    isOpen: boolean;
+    data: any;
+  }) {
+    return (
+      <div className="mb-1">
+        <button className="w-full" onClick={props.toggleAccordion}>
+          {props.title}
+          {props.isOpen ? (
+            <span className="px-2"> -</span>
+          ) : (
+            <span className="px-2"> +</span>
+          )}
+        </button>
+        {props.isOpen && (
+          <div className="w-full max-w-56 font-light text-center p-2">
+            {props.data}
+          </div>
+        )}
+      </div>
+    );
+  }
+
+  return (
+    <div className="mt-10 mb-10 lg:mt-64">
+      <div className="flex flex-col items-center justify-between h-16">
+        <div className="flex flex-col justify-center items-center py-2 text-m text-zinc-100">
+          <span className="text-4xl font-bold text-white p-5 text-center">
+            <a href="/">BENNY MANSELL </a>
+          </span>
+          <span className="flex flex-row space-x-4">
+            <a href="https://www.linkedin.com/in/benny-mansell/">
+              <img alt="Linkedin" src="./linkedin.svg" />
+            </a>
+            <a href="mailto:bennymansell49@gmail.com">
+              <img alt="Email" src="./email.svg" />
+            </a>
+          </span>
+          {/* {accordions.map((accordion) => (
+            <Accordion
+              key={accordion.key}
+              title={accordion.title}
+              data={accordion.data}
+              isOpen={accordion.isOpen}
+              toggleAccordion={() => toggleAccordion(accordion.key)}
+            />
+          ))} */}
+        </div>
+      </div>
+    </div>
+  );
+}
---
+++ app/components/Video.tsx
@@ -0,0 +1,28 @@
+"use client";
+import React, { useState } from "react";
+
+const Video = ({ src, content }: { src: string; content: any }) => {
+  const [isHovered, setIsHovered] = useState(false);
+
+  return (
+    <div className="relative flex flex-col lg:flex-row items-center py-10">
+      <div
+        className="relative group"
+        onMouseEnter={() => setIsHovered(true)}
+        onMouseLeave={() => setIsHovered(false)}
+      >
+        <video
+          className="w-[400px] h-[225px] max-w-screen aspect-video"
+          src={src}
+          aria-labelledby="videoTitle"
+          controls={isHovered} // Only show controls on hover
+        ></video>
+        <div className="absolute inset-0 flex items-center justify-center bg-black bg-opacity-50 text-white text-sm opacity-100 transition-opacity duration-300 group-hover:opacity-0 group-hover:pointer-events-none">
+          {content}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Video;
